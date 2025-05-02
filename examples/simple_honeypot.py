d7f96360c77503df6e3640e1f8d759ddd63baaf6
Yoshi2003 <cartertechsolution@gmail.com>
2025-05-02 19:14:53 -0400

    minor changes

--- examples/simple_honeypot.py
+++
@@ -1,8 +0,0 @@
-# examples/simple_honeypot.py
-from honeypot import create_honeypot_app
-
-# Create a default honeypot app
-app = create_honeypot_app()
-
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
